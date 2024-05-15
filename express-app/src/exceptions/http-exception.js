9e260fc168f97cf53b09e5807b1e4d5035b609d9
Chamith Jayaweera <chamithlkml@gmail.com>
2024-05-15 14:22:44 +0100

    Adding auth handler middleware

--- express-app/src/exceptions/http-exception.js
+++ express-app/src/exceptions/http-exception.js
@@ -11,5 +11,6 @@ export class HttpException extends Error{
 export const ErrorCode = {
   INTERNAL_ERROR: 1001,
   VALIDATION_FAILURE: 2001,
-  DATA_NOT_FOUND: 3001
+  DATA_NOT_FOUND: 3001,
+  AUTH_FAILURE: 4001
 }
--- express-app/src/server.js
+++ express-app/src/server.js
@@ -2,11 +2,12 @@ import express from 'express';
 import ErrorHandler from './middlewares/error-handler.js';
 import rootRouter from './routes/index.js';
 import { PORT } from '../config/index.js';
+import { authRequest } from './middlewares/auth-handler.js';
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', rootRouter);
+app.use('/api', [authRequest], rootRouter);
 
 app.use(ErrorHandler);
 
